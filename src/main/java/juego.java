a78d717b0a21dd4c49978a3f6d07e00c1d3e4fb1
pcast <pcast@DESK-E>
2023-08-04 14:13:45 -0600

    label

--- src/main/java/juego.java
+++ src/main/java/juego.java
@@ -1,4 +1,6 @@
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -14,13 +16,22 @@ import javax.swing.JPanel;
  * @author pcast
  */
 public class juego {
-         private JButton[][] matrizLabel;
+    private JLabel[][] matrizLabel;
 
     public void GridLayout(JPanel tab) {
         int filas = 9;
         int col = 9;
         GridLayout gridLayout = new GridLayout(filas, col);
         tab.setLayout(gridLayout);
-        matrizLabel = new JButton[filas][col];
+        matrizLabel = new JLabel[filas][col];
+         for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < col; j++) {
+                JLabel label = new JLabel();
+                label.setPreferredSize(new Dimension(50, 100));
+                matrizLabel[i][j] = label;
+                tab.add(label);
+                label.setBackground(Color.red);
+            }
+         }
     }
 }
